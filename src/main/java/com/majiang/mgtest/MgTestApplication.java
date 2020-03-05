c742a1d31b1fc4c6d8e5f612731d2a35fc120436
G W C <gwc1921978091@163.com>
2020-03-05 13:21:57 +0800

    第一次提交

---
+++ src/main/java/com/majiang/mgtest/MgTestApplication.java
@@ -0,0 +1,13 @@
+package com.majiang.mgtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MgTestApplication.class, args);
+    }
+
+}
